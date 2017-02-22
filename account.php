6b49db2f62004cbd1d642efa9c99e8c4866976d5
Joseph Choi <jc566@njit.edu>
2017-02-22 15:19:59 -0500

    The work we did on Tues

---
+++ account.php
@@ -0,0 +1,9 @@
+<?php
+
+$hostname = "sql.njit.edu"   ;
+$username = "jc566" ;
+$project  = "jc566"   ;
+$password = "SQWzKTb9" ;
+$db = "jc566";
+
+?>
--- login.php
+++ login.php
@@ -1,14 +1,50 @@
 <?php
-
-$hostname = "localhost";
-$username = "root";
-$password = "mysql";
+include (  "account.php"     );
 
 
-( $dbh = mysql_connect ( $hostname, $username, $password ) )
-            or die ( "Unable to connect to MySQL database" );
-print "Connected to MySQL<br><br>";
-mysql_select_db( $dbh, $project );
+	$dbhandle = new mysqli($hostname, $username, $password, $db);
+	if ($dbhandle->connect_errno)
+	{
+		die( "Failed to connect to MySQL: (" . $dbhandle->connect_errno . ") " . $dbhandle->connect_error);
+	} 
+	else
+	{ 
+		echo "CONNECTED TO MYSQL";
+		
+		#return $dbhandle;
+	}
+
+$user = $_POST["un"];
+$pass = $_POST["pw"];
+
+$selectStmt = $dbhandle->prepare("SELECT username,password FROM login WHERE username=$user and password=$pass");
+if(!selectStmt){
+	die("failed")
+}
+if(!(selectStmt->bind_param("ss",$user,$pass)){
+	echo "Binding param failed";
+}
+$selectStmt->execute();
+$selectStmt->store_result();
+/*
+$selectStmt = "SELECT * FROM login WHERE username='$user' and password='$pass'";
+$result = mysql_query($selectStmt);
+$count = mysql_num_rows($result);
+echo mysql_num_rows($result);
+if($count==1)
+		echo "success";
+else
+		echo "failed";
+*/
+
+/*
+FOR INSERT
+//creating a prepared statement
+$stmt = $dbhandle->prepare("INSERT INTO login (username, password,firstname, lastname) VALUES (?, ?, ?, ?)");
+
+$stmt->bind_param("ssss", $username, $password, $firstname, $lastname);*/
+	
+#$dbhandle = getConnection();
 
 
 
